9d297401b567f8589e673c53eeced1f810121914
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-21 12:35:13 -0300

    feat: change MedicoController

--- src/main/java/br/com/medline/controller/MedicoController.java
+++ src/main/java/br/com/medline/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package br.com.medline.controller;
 
+import br.com.medline.domain.medico.DadosCadastroMedicoDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedicoDTO dadosCadastroMedico){
+        System.out.println(dadosCadastroMedico);
     }
 
 }
