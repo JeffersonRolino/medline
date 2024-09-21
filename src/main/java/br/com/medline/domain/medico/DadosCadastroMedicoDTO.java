17cd0aaa62e42c792aeb150ab29031188fb163c1
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-21 14:14:50 -0300

    refactor: add validation to DadosCadastroMedicoDTO

--- src/main/java/br/com/medline/domain/medico/DadosCadastroMedicoDTO.java
+++ src/main/java/br/com/medline/domain/medico/DadosCadastroMedicoDTO.java
@@ -1,6 +1,25 @@
 package br.com.medline.domain.medico;
 
 import br.com.medline.domain.endereco.DadosEnderecoDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record DadosCadastroMedicoDTO(String nome, String email, String crm, Especialidade especialidade, DadosEnderecoDTO endereco) {
+public record DadosCadastroMedicoDTO(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        DadosEnderecoDTO endereco
+        ) {
 }
