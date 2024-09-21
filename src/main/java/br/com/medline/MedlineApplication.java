65540378095ae0773f286c593d9665f6472cc855
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-21 10:56:21 -0300

    feat: spring boot initializer

---
+++ src/main/java/br/com/medline/MedlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.medline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/medline/MedlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.medline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
