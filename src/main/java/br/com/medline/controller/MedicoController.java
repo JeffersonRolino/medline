af02604113778dcbce991de81de6fc9ca8992ff2
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-21 14:06:35 -0300

    refactor: add @Transactional to the cadastrar method

--- src/main/java/br/com/medline/controller/MedicoController.java
+++ src/main/java/br/com/medline/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package br.com.medline.controller;
 
 import br.com.medline.domain.medico.DadosCadastroMedicoDTO;
+import br.com.medline.domain.medico.Medico;
+import br.com.medline.domain.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/medicos")
 public class MedicoController {
 
+    private MedicoRepository medicoRepository;
+
+    public MedicoController(MedicoRepository medicoRepository) {
+        this.medicoRepository = medicoRepository;
+    }
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedicoDTO dadosCadastroMedico){
-        System.out.println(dadosCadastroMedico);
+        medicoRepository.save(new Medico(dadosCadastroMedico));
     }
 
 }
