108c2f84d61c4374da10625d4f2b886976370624
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-21 12:33:37 -0300

    feat: create DTO DadosEnderecoDTO

---
+++ src/main/java/br/com/medline/domain/endereco/DadosEnderecoDTO.java
@@ -0,0 +1,4 @@
+package br.com.medline.domain.endereco;
+
+public record DadosEnderecoDTO(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+}
